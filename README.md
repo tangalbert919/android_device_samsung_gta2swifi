219c6a27aca4a93a4f5fc7dbf5131ffd130df2a2
Albert Tang <tangalbert919@gmail.com>
2018-09-28 17:36:58 -0500

    Start on gta2swifi
    The GPS was shamelessly stolen from another device tree.

---
+++ README.md
@@ -0,0 +1,20 @@
+Copyright (C) 2018 - The LineageOS Project
+
+This device tree is not recommended for use at this time.
+
+Device configuration for the Samsung Galaxy Tab (A) SM-T380 [gta2swifi]
+=============
+
+Basic   | Specs
+-------:|:---------
+SoC     | Qualcomm MSM8917 Quad-core 1.4 GHz ARM® Cortex™ A53
+Chipset | Qualcomm MSM8917 Snapdragon 425
+GPU     | Adreno 308
+RAM     | 2.0 GB
+Shipped Android Version | 6.0.1
+Storage | 16 GB
+MicroSD | Up to 256 GB
+Battery | 5000 mAh
+Display | 1200 x 800, 8.0" (203.1 mm)
+Front Camera | 5.0 MP
+Rear Camera | 8.0 MP
---
+++ lineage_gta2swifi.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/samsung/gt58wifi/device.mk)
+
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1200
+TARGET_SCREEN_WIDTH := 800
+
+PRODUCT_NAME := gta2swifi
+PRODUCT_NAME := lineage_gta2swifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T380
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
