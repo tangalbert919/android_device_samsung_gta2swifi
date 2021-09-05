e9c2e1ab17b456b16f7700abbd48b75f101dec3c
Albert Tang <tangalbert919@yahoo.com>
2021-09-05 15:26:39 -0500

    Add build desc and fingerprint

--- lineage_gta2swifi.mk
+++ lineage_gta2swifi.mk
@@ -14,19 +14,27 @@
 # limitations under the License.
 #
 
+# Inherit from gta2swifi device
 $(call inherit-product, device/samsung/gta2swifi/device.mk)
 
+# Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1200
 TARGET_SCREEN_WIDTH := 800
 
-PRODUCT_NAME := gta2swifi
-PRODUCT_NAME := lineage_gta2swifi
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-T380
+PRODUCT_DEVICE := gta2swifi
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_NAME := lineage_gta2swifi
+PRODUCT_MODEL := SM-T380
 PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+TARGET_VENDOR := samsung
+TARGET_VENDOR_PRODUCT_NAME := gta2swifi
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="gta2swifidx-user 9 PPR1.180610.011 T380DXS4CUD3 release-keys"
+
+BUILD_FINGERPRINT := "samsung/gta2swifidx/gta2swifi:9/PPR1.180610.011/T380DXS4CUD3:user/release-keys"
