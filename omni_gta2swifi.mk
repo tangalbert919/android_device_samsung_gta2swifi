dcf457a2369292fc5f53a23f70c146fa609d9da7
Albert Tang <tangalbert919@yahoo.com>
2022-01-17 20:08:31 -0800

    Finish the tree

--- omni_gta2swifi.mk
+++ omni_gta2swifi.mk
@@ -6,16 +6,12 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from gta2swifi device
-$(call inherit-product, device/samsung/gta2swifi/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gta2swifi
@@ -23,3 +19,4 @@ PRODUCT_NAME := omni_gta2swifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T380
 PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := tablet
